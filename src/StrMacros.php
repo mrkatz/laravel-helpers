e465bbcbc92991ede12e8863c114061bfabed4a0
Adam Kaczocha <adamkaczocha@gmail.com>
2019-11-10 22:44:02 +1100

    Fixed Str Macro Errors

--- src/StrMacros.php
+++ src/StrMacros.php
@@ -9,109 +9,129 @@ use Illuminate\Support\Facades\Validator;
 class StrMacros
 {
     /**
-     * Returns string netween two other strings
-     *
-     * @param string $subject
-     * @param string $beginning
-     *
-     * @param null|string $end
+     * Returns string between two other strings
      *
      * @return string
      *
      * @example  Str::between('--thing--', '--'); // returns "thing"
      * @example  Str::between('[thing]', '[', ']'); // returns "thing"
      */
-    public function between($subject, $beginning, $end = null)
+    public function between()
     {
-        if (is_null($end)) {
-            $end = $beginning;
-        }
-
-        return Str::before(Str::after($subject, $beginning), $end);
+        /**
+         * @param $subject
+         * @param $beginning
+         * @param null $end
+         *
+         * @return string
+         */
+        return function ($subject, $beginning, $end = null) {
+            if (is_null($end)) {
+                $end = $beginning;
+            }
+            return Str::before(Str::after($subject, $beginning), $end);
+        };
     }
 
     /**
      * Returns capture groups contained in the provided regex pattern.
      *
-     * @param string $string
-     * @param string $pattern
-     *
      * @return mixed|null
      *
      * @example Str::extract('Jan-01-2019', '/Jan-(.*)-2019/'); // returns "01"
      */
-    public function extract(string $string, string $pattern)
+    public function extract()
     {
-        if (@preg_match($pattern, $string) === false) {
-            $pattern = '#' . preg_quote($pattern, '#') . '#';
-        }
-
-        preg_match($pattern, $string, $matches);
-
-        return $matches[1] ?? null;
+        /**
+         * @param $string
+         * @param $pattern
+         *
+         * @return mixed|null
+         */
+        return function ($string, $pattern) {
+            if (@preg_match($pattern, $string) === false) {
+                $pattern = '#'.preg_quote($pattern, '#').'#';
+            }
+            preg_match($pattern, $string, $matches);
+            return $matches[1] ?? null;
+        };
     }
 
 
     /**
      * Checks the provided string against the provided regex pattern.
      *
-     * @param string $string
-     * @param string $pattern
-     *
-     * @return bool
+     * @return \Closure
      *
      * @example Str::match('Jan-1-2019', '/Jan-(.*)-2019/'); // returns true
      */
-    public function match(string $string, string $pattern)
+    public function match()
     {
-        if (@preg_match($pattern, $string) === false) {
-            $pattern = '#' . preg_quote($pattern, '#') . '#';
-        }
-
-        return preg_match($pattern, $string) === 1;
+        /**
+         * @param $string
+         * @param $pattern
+         *
+         * @return bool
+         */
+        return function($string, $pattern) {
+            if (@preg_match($pattern, $string) === false) {
+                $pattern = '#'.preg_quote($pattern, '#').'#';
+            }
+            return preg_match($pattern, $string) === 1;
+        };
     }
 
     /**
      * A simple way to use validate a string using Laravel's built-in validation system.
      *
-     * @param string $data
-     * @param string $rules
-     *
-     * @return Collection
+     * @return \Closure
      *
      * @example Str::validate('calebporzio@aol.com', 'regex:/\.net$/|email|max:10');
      * returns: ["Format is invalid.", "May not be greater than 10 characters."]
      */
-    public function validate(string $data, string $rules)
+    public function validate()
     {
-        return Collection::make(
-            Validator::make(['{placeholder}' => $data], ['{placeholder}' => $rules])
-                     ->errors()
-                     ->get('{placeholder}')
-        )->map(function ($message) {
-            return ucfirst(
-                trim(
-                    str_replace('The {placeholder}', '',
-                                str_replace('The selected {placeholder}', 'This selection',
-                                            $message)
+        /**
+         * @param $data
+         * @param $rules
+         *
+         * @return Collection
+         */
+        return function($data, $rules) {
+            return Collection::make(
+                Validator::make(['{placeholder}' => $data], ['{placeholder}' => $rules])
+                         ->errors()
+                         ->get('{placeholder}')
+            )->map(function ($message) {
+                return ucfirst(
+                    trim(
+                        str_replace('The {placeholder}', '',
+                                    str_replace('The selected {placeholder}', 'This selection',
+                                                $message)
+                        )
                     )
-                )
-            );
-        });
+                );
+            });
+        };
     }
 
     /**
      * Wrap a string within another string
      *
-     * @param string $value
-     * @param string $cap
-     *
      * @return string
      *
      * @example Str::wrap('thing', '--'); // returns "--thing--"
      */
-    public function wrap(string $value, string $cap)
+    public function wrap()
     {
-        return Str::start(Str::finish($value, $cap), $cap);
+        /**
+         * @param $value
+         * @param $cap
+         *
+         * @return string
+         */
+        return function($value, $cap) {
+            return Str::start(Str::finish($value, $cap), $cap);
+        };
     }
 }
